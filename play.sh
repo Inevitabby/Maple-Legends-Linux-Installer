8f9dc177ee784816817cc1088fb70cbd750c9a6f
Inevitabby 🐶 <161525738+Inevitabby@users.noreply.github.com>
2025-05-21 21:19:10 +0000

    tweak: Add WINE_DPI_SCALE to top of file
    96 DPI is default scaling, this is just to make it easier for users who want to try increasing it.
    
    Haven't tested higher DPI on my machine yet, but looks promising.

--- play.sh
+++ play.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")" || exit
 
 export WINEPREFIX="$(realpath .wine)"
 export WINEARCH="win32"
+export WINE_DPI_SCALE=96 # scale_factor = WINE_DPI_SCALE / 96
 
 if [[ ! -d "${WINEPREFIX}" ]]; then
 	echo "Please run setup.sh first (WINEPREFIX not found)"
