5d6a9f6144c2fb52db221040d853a936535e0560
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2026-02-07 20:14:18 -0800

    tweak: Remove last bit of Winetricks use
    To keep things portable though (to prevent future breakages like what
    happened with WoW64 WINE), I'm removing Winetricks.
    
    Winetricks wasn't ever required to create a working prefix, I just
    used it for corefonts really.

--- setup.sh
+++ setup.sh
@@ -8,29 +8,26 @@ readonly RES="1" # 0 = 800x600, 1 = 1024x768, 2 = 1366x768 (potentially unstable
 source .util/setup_checks.sh
 source .util/ensure_wine.sh
 
-step "1. Extracting game files from ${PKG} (this may take a while)... [1/7]"
+step "1. Extracting game files from ${PKG} (this may take a while)... [1/6]"
 bsdtar -xO --fast-read --file "$PKG" "MapleLegends.pkg/Payload" | \
 	bsdtar -x --strip-components=5 "MapleLegends.app/Contents/Resources/drive_c/MapleLegends"
 
-step "2. Creating 32-bit Wine prefix... [2/7]"
+step "2. Creating 32-bit Wine prefix... [2/6]"
 wineboot --init
 wineserver -w
 
-step "3. Installing Corefonts... [3/7]"
-# winetricks -q corefonts # vcrun6sp6 dxvk vkd3d
-
-step "4. Installing networking DLLs... [4/7]"
+step "3. Installing networking DLLs... [3/6]"
 add_dll_override "ws2help"
 add_dll_override "ws2_32"
 
-step "5. Setting Virtual Desktop and WINE_DPI_SCALE... [5/7]"
+step "4. Setting Virtual Desktop and WINE_DPI_SCALE... [4/6]"
 reg_add "HKCU\\Software\\Wine\\Explorer" "Desktop" "Default"
 set_resolution "${RES}"
 
-step "6. Setting Windows Version... [6/7]"
-winetricks -q win98
+step "5. Setting Windows Version... [5/6]"
+reg_add "HKCU\\Software\\Wine" "Version" "win98"
 
-step "7. Linking MapleLegends folder to Wineprefix [7/7]"
+step "6. Linking MapleLegends folder to Wineprefix [6/6]"
 ln -s "$(realpath "MapleLegends")" "${WINEPREFIX}/drive_c/MapleLegends"
 ini_set "SkipLogoAnimation" "true"
 
