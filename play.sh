812eddf7c9a56574b91339665d896b86ae8f4386
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2026-02-07 23:54:45 -0800

    tweak: exit earlier if WINEPREFIX missing
    previously we were hitting ensure_wine first, which would've made it
    install wine and then fail to run the game

--- play.sh
+++ play.sh
@@ -1,12 +1,10 @@
 #!/bin/env bash
 cd "$(dirname "$0")" || exit
+
+export WINEPREFIX="$(realpath .wine)"
+[[ -d "${WINEPREFIX}" ]] || { echo "Please run setup.sh first (WINEPREFIX not found)"; exit 1; }
+
 source .util/ensure_wine.sh
-
-if [[ ! -d "${WINEPREFIX}" ]]; then
-	echo "Please run setup.sh first (WINEPREFIX not found)"
-	exit 1
-fi
-
 source .dpi.sh
 
 wine start /unix "${WINEPREFIX}/drive_c/MapleLegends/MapleLegends.exe"
